8b459664f9257e47717bbe6be9eaca530060ea22
jordan quiqueret <jordan.quiqueret@gmail.com>
2024-03-06 14:32:06 +0100

    donation bar

---
+++ app/bar/page.js
@@ -0,0 +1,181 @@
+const BarPage = () => {
+     return (
+          <div className="bg-white text-black">
+               <div className="flex flex-col items-center">
+                    <div>
+                         <img
+                              src="https://www.makesenserecords.com/backstagecontent/SCRIPT_LOGO_NOIR.png"
+                              alt="script-club logo"
+                              className="w-15 m-auto"
+                         />
+                    </div>
+                    <div className="text-center">
+                         DONATION BAR
+                         <br />
+                         PRESS ESC TO BROWSE AND CLICK TO BUY
+                    </div>
+                    <div></div>
+                    <p>&nbsp;</p>
+                    <ul>
+                         <li>
+                              <h1>
+                                   <a
+                                        href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                        target="new"
+                                   >
+                                        Coat Check
+                                   </a>{" "}
+                                   .............................................
+                                   2,50
+                              </h1>
+                         </li>
+                    </ul>
+                    <ul>
+                         <li>
+                              <h1>
+                                   <a
+                                        href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                        target="new"
+                                   >
+                                        A.I.neken
+                                   </a>{" "}
+                                   .....................................................
+                                   5{" "}
+                              </h1>
+                              <p>&nbsp;</p>
+                              <h3> COCKTAILS </h3>
+                         </li>
+                         <li>
+                              <h1>
+                                   <a
+                                        href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                        title="Heming.wav - donation"
+                                        target="new"
+                                   >
+                                        Heming.wav
+                                   </a>{" "}
+                                   ..............................................
+                                   10{" "}
+                              </h1>
+                         </li>
+                         <li>
+                              <h1>
+                                   <a
+                                        href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                        title="bloody wifi - donation"
+                                        target="new"
+                                   >
+                                        Bloody Wifi
+                                   </a>{" "}
+                                   ...............................................
+                                   10
+                              </h1>
+                         </li>
+                         <li>
+                              <h1>
+                                   <a
+                                        href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                        title="sex on the byte - donation"
+                                        target="new"
+                                   >
+                                        Sex On the Bits
+                                   </a>{" "}
+                                   ......................................... 10{" "}
+                              </h1>
+                         </li>
+                    </ul>
+                    <blockquote>
+                         <p>&nbsp;</p>
+                         <h3>
+                              All price in € - Scroll Down to see cocktails
+                              (computer only)
+                         </h3>
+                         <p>&nbsp;</p>
+                         <ul>
+                              <li>
+                                   <p>
+                                        <img
+                                             src="https://www.makesenserecords.com/hemingwav.jpeg"
+                                             width="500"
+                                             height="500"
+                                             alt=""
+                                        />
+                                   </p>
+                              </li>
+                              <li>
+                                   <h1>
+                                        <a
+                                             href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                             title="Heming.wav - donation"
+                                             target="new"
+                                        >
+                                             Heming.wav
+                                        </a>{" "}
+                                        ..............................................
+                                        10{" "}
+                                   </h1>
+                                   <p>&nbsp;</p>
+                                   <img
+                                        src="https://www.makesenserecords.com/bloody.jpeg"
+                                        width="500"
+                                        height="500"
+                                        alt=""
+                                   />
+                              </li>
+                         </ul>
+                         <ul>
+                              <li>
+                                   <h1>
+                                        <a
+                                             href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                             title="bloody wifi - donation"
+                                             target="new"
+                                        >
+                                             Bloody Wifi
+                                        </a>{" "}
+                                        ...............................................
+                                        10
+                                   </h1>
+                                   <p>&nbsp;</p>
+                                   <p>
+                                        <img
+                                             src="https://www.makesenserecords.com/sexbits.jpeg"
+                                             width="500"
+                                             height="500"
+                                             alt=""
+                                        />
+                                   </p>
+                              </li>
+                              <li>
+                                   <h1>
+                                        <a
+                                             href="https://www.paypal.com/donate?hosted_button_id=ZCY9K9VSHVDAQ"
+                                             title="sex on the byte - donation"
+                                             target="new"
+                                        >
+                                             Sex On the Bits
+                                        </a>{" "}
+                                        .........................................
+                                        10{" "}
+                                   </h1>
+                              </li>
+                         </ul>
+                         <blockquote>
+                              <p>&nbsp;</p>
+                              <p>&nbsp;</p>
+                              <p>Thanks for your support,</p>
+                              <p>&nbsp;</p>
+                              <p>&nbsp;</p>
+                              <p>La Direction.</p>
+                         </blockquote>
+                         <p></p>
+                         <p>&nbsp;</p>
+                         <p>&nbsp;</p>
+                    </blockquote>
+                    <p>&nbsp;</p>
+               </div>
+          </div>
+     );
+};
+
+export default BarPage;
