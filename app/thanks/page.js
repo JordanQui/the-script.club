1c0de2a2d41116b817bf39f16c999c999f75bb08
jordan quiqueret <jordan.quiqueret@gmail.com>
2024-03-07 16:41:31 +0100

    update

--- app/thanks/page.js
+++ app/thanks/page.js
@@ -1,7 +1,7 @@
 const ThanksPage = () => {
      return (
           <div className="bg-white text-black text-xl h-screen flex items-center flex-col justify-center">
-               <h1>THANKS YOU</h1>
+               <h1>THANK YOU</h1>
                <span>❤️</span>
           </div>
      );
